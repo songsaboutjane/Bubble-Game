fc95db7eb3a76a3b9d119973c22ec852f571db45
ryantoronto <ryan@pwportfolioanalytics.com>
2018-02-12 16:37:04 -0500

    Portal Animation

---
+++ Bubbles/Assets/_scripts/PortalScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (NewMoveHero.gotKey == true) {
+			anim.SetTrigger ("Activon");
+		}
+	}
+}
