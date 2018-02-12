8f8d685aa01fb591855667862da378b01543754b
Cassidy Houlding <choulding@STUDENT-choulding.local>
2018-02-11 19:25:40 -0500

    hewk

---
+++ Bubbles/Assets/_scripts/moveTotem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTotem : MonoBehaviour {
+	private Rigidbody2D rb2d;
+	public float maxh=0f;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (onpush.ispushed == true) {
+			rb2d.MovePosition (rb2d.position + new Vector2 (0, -2) * Time.fixedDeltaTime);
+		}
+	}
+}
---
+++ Bubbles/Assets/_scripts/onpush.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onpush : MonoBehaviour {
+	public static bool ispushed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionStay2D (Collision2D hit) {
+		if (hit.gameObject.tag.Equals ("weighted") == true) {
+			ispushed = true;
+		} else {
+			ispushed = false;
+		}
+	}
+}
