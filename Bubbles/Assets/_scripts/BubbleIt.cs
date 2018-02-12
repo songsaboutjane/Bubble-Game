3244a70ba137e3c387dfa96b670c2ad5e73a50ca
ryantoronto <ryan@pwportfolioanalytics.com>
2018-02-11 19:33:49 -0500

    mass script changes

--- Bubbles/Assets/_scripts/BubbleIt.cs
+++ Bubbles/Assets/_scripts/BubbleIt.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class BubbleIt : MonoBehaviour {
 	private Rigidbody2D rb2d;
-	public float mass = 1f;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
-
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		//rb2d.mass = mass;
-		
-	}
+	void Update () {}
 
 	void OnTriggerStay2D (Collider2D targ) {
-		
-		if(targ.gameObject.tag.Equals("Bubble") == true ){
+		if(targ.gameObject.tag.Equals("Bubble") == true ) {
 			rb2d.mass = 0.0f;
-
 		}
+	}
 
+	void OnCollisionExit2D () {
+		rb2d.mass = 1f;
 	}
 
 
--- Bubbles/Assets/_scripts/HeroBubbleIt.cs
+++ Bubbles/Assets/_scripts/HeroBubbleIt.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class HeroBubbleIt : MonoBehaviour {
 	private Rigidbody2D rb2d;
-	public float mass = 1f;
 	private NewMoveHero newmoveHero;
-	private bool inBubble = false;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -14,32 +12,17 @@ public class HeroBubbleIt : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (inBubble == false) {
-			rb2d.mass = mass;
-			newmoveHero.enabled=true;
-		}
-		//newmoveHero.enabled=true;
-	}
+	void Update () {}
 
 	void OnTriggerStay2D (Collider2D targ) {
 		if (targ.gameObject.tag.Equals ("Bubble") == true && targ.GetType() == typeof(CircleCollider2D)) {
-			inBubble = true;
 			rb2d.mass = 0.0f;
 			newmoveHero.enabled = false;
-			Debug.Log ("Touched the bubble");
-
 		} 
-		else {
-			inBubble = false;
-			newmoveHero.enabled=true;
-			Debug.Log("HEre");
-		}
-
 	}
-	void OnCollisionEnter2D () {
+
+	void OnCollisionExit2D () {
+		rb2d.mass = 1f;
 		newmoveHero.enabled=true;
 	}
-
-
 }
