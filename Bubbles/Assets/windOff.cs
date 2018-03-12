b7c9d69c8d81dad4785e1ca0134b9d7caaa3de4d
Cassidy Houlding <choulding@STUDENT-choulding.local>
2018-03-11 23:58:29 -0400

    broken and terrible

--- Bubbles/Assets/windOff.cs
+++ Bubbles/Assets/windOff.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class windOff : MonoBehaviour {
 	private AreaEffector2D ae2d;
+	public float force;
 
 	// Use this for initialization
 	void Start () {
-		ae2d = GetComponent<AreaEffector2D> ();
+		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (leveron.lever == true) {
-			Destroy (ae2d);
+			Destroy (gameObject);
 		}
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.gameObject.CompareTag("weighted") || col.gameObject.CompareTag("Bubble")) {
+			Debug.Log ("Heyo");
+			col.attachedRigidbody.AddForce (new Vector2 (force, 0));
+		}
+	
 	}
 }
